3c14358258f48b715f8844e79b229bbde1a18f4c
darkangelsinner <das.log@protonmail.com>
2023-10-21 21:55:48 -0500

    Constructores
    - se agregan posibles constructores
    - se agrega el primer método de calculo de fechas

--- Edad.java
+++ Edad.java
@@ -1,3 +1,46 @@
-public class Edad{
-    
+
+public class Edad {
+    // creamos constructores para cada caso
+
+    private int anio, mes, dia, edad;
+    private int segundoAnio, segundoMes, SegundoDias;
+
+    // casos de fecha completa
+
+    public Edad(int anioNac, int mesNac, int diaNac) {
+        this.anio = anioNac;
+        this.mes = mesNac;
+        this.dia = diaNac;
+    }
+
+    // caso que solo proporcionen anio
+
+    public Edad(int anioNac) {
+        this.anio = anioNac;
+        this.mes = 0;
+        this.dia = 0;
+
+    }
+
+    // caso que no tenga ni un anio
+
+    public Edad(int mesNac, int diaNac) {
+        this.anio = 0;
+        this.mes = mesNac;
+        this.dia = diaNac;
+    }
+
+    // calcular fecha de nacimiento con edad proporcionada
+
+    public Edad(byte edad) {
+        this.edad = edad;
+    }
+
+    // metodo para calcular edad por medio de dos fechas completas
+    public void calcularEdadCompleta(int segundoAnio, int segundoMes, int SegundoDias) {
+
+        //fecha actual - fecha nacimiento, pero en logica hay una condicion de que si aun no llega la fecha de cumpleanos se resta un anio
+
+    }
+
 }
