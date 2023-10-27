980547108ebed5da5cc93af22372f5a00d56b1d7
BryanTuz <bryantuz05@gmail.com>
2023-10-27 00:36:55 -0500

    mi parte

--- Edad.java
+++ Edad.java
@@ -6,7 +6,7 @@ public class Edad {
     //variables
 
     private int anio, mes, dia, edad;
-    private int segundoAnio, segundoMes, SegundoDias;
+    private int segundoAnio, segundoMes, segundoDias;
 
     // casos de fecha completa
 
@@ -39,15 +39,129 @@ public class Edad {
         this.edad = edad;
     }
 
+    public String calcularEdadDias(int segundoAnio, int segundoMes, int segundoDias){
+        
+
+        int edadAn = 0;
+        int edadMes = segundoMes - mes;
+
+        //para calcular la edad de anios
+        if (segundoMes < mes){
+            edadAn = (segundoAnio - anio) - 1;
+        }
+
+        if (segundoMes == mes && segundoDias < dia){
+            edadAn = (segundoAnio - anio) - 1;
+        }
+        edadAn = (segundoAnio - anio);
+
+        //para calcular los dias
+        int diaRe = segundoDias - dia;
+        if (diaRe < 0){
+        diaRe = (30 + segundoDias) - dia;
+        //mes = mes + 1;
+
+        edadMes = segundoMes - mes;   
+    }
+        //Calcular los meses
+    if(edadMes < 0){
+        edadMes = edadMes + 12;
+    }
+
+        int edadDias = (edadAn * 365) + (edadMes * 30) + diaRe;
+
+        return ("Su edad en dias son: " + edadDias);
+    }
+
     // metodo para calcular edad por medio de dos fechas completas
-    public void calcularEdadCompleta(int segundoAnio, int segundoMes, int SegundoDias) {
+    public String calcularEdadCompleta(int segundoAnio, int segundoMes, int segundoDias) {
 
 
         //fecha actual - fecha nacimiento, pero en logica hay una condicion de que si aun no llega la fecha de cumpleanos se resta un anio
         // No quiero usar localDate porque hay que desarrollar la logica
+        int edadAn = 0;
 
-        //jdjjdjsdjdsdss
+        if (segundoMes < mes){
+            edadAn = (segundoAnio - anio) - 1;
+        }
 
+        if (segundoMes == mes && segundoDias < dia){
+            edadAn = (segundoAnio - anio) - 1;
+        }
+        else {
+            edadAn = (segundoAnio - anio);
+        }
+
+        int diaRe = segundoDias - dia;
+        if (diaRe < 0){
+        diaRe = (30 + segundoDias) - dia;
+        mes = mes + 1;
+        }
+
+        int edadMes = segundoMes - mes;
+        if(edadMes < 0){
+            edadMes = edadMes + 12;
+        }
+
+        if (edadAn == 0) {
+            return ("La edad completa es: " + edadMes + " Mes(es) " + diaRe + " Dia(s)");
+        }   else if (edadMes == 0){
+            return ("La edad completa es: " + edadAn + " Año(s) " + diaRe + " Dia(s)");
+        } else if (diaRe == 0) {
+            return ("La edad completa es: " + edadAn + " Año(s) " + edadMes + " Mes(es) ");
+        } else {
+            return ("La edad completa es: " + edadAn + " Año(s) " + edadMes + " Mes(es) " 
+        + diaRe + " Dia(s)");
+        }
+        
+
+    }
+
+    public String calcularEdadAnio(int segundoAnio, int segundoMes, int segundoDias){
+        
+        int edadAn = 0;
+
+        if (segundoMes < mes){
+            edadAn = (segundoAnio - anio) - 1;
+        }
+
+        else if (segundoMes == mes && segundoDias < dia){
+            edadAn = (segundoAnio - anio) - 1;
+        }
+
+        edadAn = (segundoAnio - anio);
+        return ("La edad en años es: " + edadAn + " Año(s)");
+    }
+
+
+    public String calcularEdadMeses(int segundoAnio, int segundoMes, int segundoDias){
+
+        int edadAn = 0;
+        int edadMes = segundoMes - mes;
+
+        if (segundoMes < mes){
+            edadAn = (segundoAnio - anio) - 1;
+        }
+
+        if (segundoMes == mes && segundoDias < dia){
+            edadAn = (segundoAnio - anio) - 1;
+        }
+        edadAn = (segundoAnio - anio);
+
+        int diaRe = segundoDias - dia;
+        if (diaRe < 0){
+        diaRe = (30 + segundoDias) - dia;
+        //mes = mes + 1;
+
+        edadMes = segundoMes - mes;   
+    }
+        
+    if(edadMes < 0){
+        edadMes = edadMes + 12;
+    }
+
+        edadMes = (edadAn * 12) + edadMes;
+        return ("Su edad en meses son: " + edadMes);
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Main{
     public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        Edad edad1;
+
+        System.out.println("Ingrese la fecha de nacimiento(dia/mes/año):");
+        int diaNac = read.nextInt();
+        int mesNac = read.nextInt();
+        int anioNac = read.nextInt();
+
+        System.out.println(diaNac +"/" + mesNac +"/" + anioNac);
+        edad1 = new Edad(anioNac, mesNac, diaNac);
+
+        System.out.println("Ingrese la fecha actual(dia/mes/año):");
+        int diaAc = read.nextInt();
+        int mesAc = read.nextInt();
+        int anioAc = read.nextInt();
+        System.out.println(diaAc + "/" + mesAc + "/" + anioAc);
+        
+        System.out.println(edad1.calcularEdadCompleta(anioAc, mesAc, diaAc) + "\n" +
+        edad1.calcularEdadAnio(anioAc, mesAc, diaAc) + "\n" +
+        edad1.calcularEdadDias(anioAc, mesAc, diaAc) + "\n" +
+        edad1.calcularEdadMeses(anioAc, mesAc, diaAc));
 
     }
 }
