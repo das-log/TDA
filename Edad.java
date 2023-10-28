a0f9ee6243261d144b70f44dc81ea96c14ca4081
BryanTuz <bryantuz05@gmail.com>
2023-10-27 19:44:58 -0500

    avance

--- Edad.java
+++ Edad.java
@@ -1,32 +1,19 @@
-
 public class Edad {
-    // creamos constructores para cada caso
-
-    // variables
-
-    private int anio, mes, dia; // variables locales recibidas del nacimiento
-    private int segundoAnio, segundoMes, segundoDias; // variables para fecha actual o fechas posterior
-    private int edadAnios, edadMeses, edadDias; // para almacenar edad segun calculos
-
-    // casos de fecha completa
+    private int anio, mes, dia;
+    private int segundoAnio, segundoMes, segundoDias;
+    private int edadAnios, edadMeses, edadDias;
 
     public Edad(int anioNac, int mesNac, int diaNac) {
         this.anio = anioNac;
         this.mes = mesNac;
         this.dia = diaNac;
-
     }
-
-    // caso que solo proporcionen anio
 
     public Edad(int anioNac) {
         this.anio = anioNac;
         this.mes = 0;
         this.dia = 0;
-
     }
-
-    // caso que no tenga ni un anio
 
     public Edad(int mesNac, int diaNac) {
         this.anio = 0;
@@ -34,71 +21,35 @@ public class Edad {
         this.dia = diaNac;
     }
 
-    // calcular fecha de nacimiento con edad proporcionada
-
     public Edad(byte edad) {
         this.edadAnios = edad;
     }
 
-    /**me acabo de dar cuenta que aqui hay que usar un objeto o un arreglo para 
-    retornar las variables asi que hay que pasar de void a int arregloEdad[3] y retornar valores en el arrelgo usando 
-    arregloEdad[0] = edadAnios
-    arregloEdad[1] = edadMeses
-    arregloEdad[2] = edadDias
-    **/
+    public int[] calcularEdadCompleta(int segundoAnio, int segundoMes, int segundoDias) {
+        int[] arregloEdad = new int[3];
 
-    // metodo para calcular edad por medio de dos fechas completas
-    public void calcularEdadCompleta(int segundoAnio, int segundoMes, int segundoDias) {
-
-        //**aqui donde el mes es el mismo de ambos
-        if (mes == segundoMes && dia == segundoDias) { // si es su cumpleanio
-            edadAnios = segundoAnio - anio;
-
-            //retorna edadAnios;**
-         
-        } else if (mes == segundoMes && dia < segundoDias) { // si ya cumplio dentro del mes
-            edadAnios = segundoAnio - anio;
-            edadDias = segundoDias - dia;
-            //debe retornar ambos**
-
-        } else if (mes == segundoMes && dia > segundoDias) {// si esta por cumplir
-            edadAnios = segundoAnio - anio;
-
-            edadMeses = segundoMes - mes;
-
-            edadDias = segundoDias - dia;
-
-            //debe retornar los 3**
-
-        }else {   //**  si el mes es diferente
-               
-            if (segundoMes < mes ){ //para evitar mes negativo
-                edadAnios = segundoAnio - anio - 1;
-                edadMeses = 12 - mes + segundoMes;
-                if(segundoDias < dia){              //para evitar dias negativos
-                    edadDias = dia - segundoDias;
-                    
-                } else {
-                    edadDias = segundoDias - dia;
-                    
-                }
-
-                //debe retornar los 3**
-
-            } else {
-                edadAnios = segundoAnio - anio;
-                edadMeses = segundoMes - mes;
-                edadDias = segundoDias - dia;
-
-                //debe retornar los 3**
-
-            }
-
-            
-        }
-
-       
-
+        // Ajuste si el día de nacimiento es mayor que el día actual
+    if (segundoDias < dia) {
+        segundoMes--;
+        segundoDias += 30; // Suponemos un mes de 30 días
     }
 
+    // Ajuste si el mes de nacimiento es mayor que el mes actual
+    if (segundoMes < mes) {
+        segundoAnio--;
+        segundoMes += 12; 
+    }
+
+    edadAnios = segundoAnio - anio;
+    edadMeses = segundoMes - mes;
+    edadDias = segundoDias - dia;
+
+    arregloEdad[0] = edadAnios;
+    arregloEdad[1] = edadMeses;
+    arregloEdad[2] = edadDias;
+
+    
+
+        return arregloEdad;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,36 @@
-public class Main{
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicitar y obtener la fecha de nacimiento del usuario
+        System.out.print("Ingresa el año de nacimiento: ");
+        int anioNac = scanner.nextInt();
+
+        System.out.print("Ingresa el mes de nacimiento: ");
+        int mesNac = scanner.nextInt();
+
+        System.out.print("Ingresa el día de nacimiento: ");
+        int diaNac = scanner.nextInt();
+
+        // Crear una instancia de Edad con la fecha de nacimiento proporcionada
+        Edad edad = new Edad(anioNac, mesNac, diaNac);
+
+        // Solicitar y obtener la fecha posterior del usuario
+        System.out.print("Ingresa el año actual: ");
+        int segundoAnio = scanner.nextInt();
+
+        System.out.print("Ingresa el mes actual: ");
+        int segundoMes = scanner.nextInt();
+
+        System.out.print("Ingresa el día actual: ");
+        int segundoDias = scanner.nextInt();
+
+        // Calcular la edad completa con la fecha posterior ingresada
+        int[] edadCalculada = edad.calcularEdadCompleta(segundoAnio, segundoMes, segundoDias);
+
+        // Mostrar la edad calculada
+        System.out.println("Edad calculada: " + edadCalculada[0] + " años, " + edadCalculada[1] + " meses, " + edadCalculada[2] + " días");
     }
 }
