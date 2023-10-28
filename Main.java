d4e2ff3fabf24bffebe6774171f9a8a18ade9859
darkangelsinner <das.log@protonmail.com>
2023-10-27 20:12:58 -0500

    Comentario constructor
    - modificar constructores
    - checar main

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+         /***AQUI HAY QUE MOVER ESTO AL CONSTRUCTOR PORQUE SI SE CREA EL OBJETO CON EL CONSTRUCTOR SOLO ANIO SOLO SE PEDIRIA ANIO, AQUI ESTAS PIDIENDO LOS 3 */
         // Solicitar y obtener la fecha de nacimiento del usuario
         System.out.print("Ingresa el año de nacimiento: ");
         int anioNac = scanner.nextInt();
@@ -13,9 +14,13 @@ public class Main {
 
         System.out.print("Ingresa el día de nacimiento: ");
         int diaNac = scanner.nextInt();
+       /***HASTA AQUI */
 
+       
         // Crear una instancia de Edad con la fecha de nacimiento proporcionada
         Edad edad = new Edad(anioNac, mesNac, diaNac);
+
+
 
         // Solicitar y obtener la fecha posterior del usuario
         System.out.print("Ingresa el año actual: ");
@@ -27,6 +32,7 @@ public class Main {
         System.out.print("Ingresa el día actual: ");
         int segundoDias = scanner.nextInt();
 
+        
         // Calcular la edad completa con la fecha posterior ingresada
         int[] edadCalculada = edad.calcularEdadCompleta(segundoAnio, segundoMes, segundoDias);
 
