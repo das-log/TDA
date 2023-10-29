e0bb1932d0ec17bae114ca354c1ff71c6573848d
darkangelsinner <das.log@protonmail.com>
2023-10-29 15:51:13 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -20,22 +20,22 @@ public class Main {
             System.out.println("2. Crear edad con solo año");
             System.out.println("3. Crear edad con solo meses y días");
             System.out.println("4. Crear edad con edad en años");
-        
+
             byte opcion = scanner.nextByte(); // Objeto tipo Scanner para ingresar opción en switch
-        
+
             /* Variables que se usan para almacenar valores leídos por el escáner. */
             int anioNac; // representa el año de nacimiento
             int mesNac; // representa el mes de nacimiento
             int diaNac; // representa el día de nacimiento
             int edadEnAnios; // representa la edad en años si se ingresa como constructor
             int segundoAnio, segundoMes, segundoDias; // representa datos de la segunda fecha
-        
+
             /*
              * Este switch tiene la función de pedir datos según lo requerido por el
              * usuario. Por lo tanto, el objeto de tipo Edad procesará datos según el
              * constructor de cada caso
              */
-        
+
             switch (opcion) {
                 case 1:
                     /*
@@ -48,12 +48,12 @@ public class Main {
                         mesNac = scanner.nextInt();
                         System.out.println("Ingrese el día de nacimiento:");
                         diaNac = scanner.nextInt();
-        
+
                         if (mesNac < 1 || mesNac > 12 || diaNac < 1 || diaNac > 30) {
                             System.out.println("Valores fuera de rango");
                         }
                     } while (mesNac < 1 || mesNac > 12 || diaNac < 1 || diaNac > 30);
-        
+
                     edad = new Edad(anioNac, mesNac, diaNac);
                     break;
                 case 2:
@@ -67,38 +67,37 @@ public class Main {
                         mesNac = scanner.nextInt();
                         System.out.println("Ingrese el día de nacimiento:");
                         diaNac = scanner.nextInt();
-        
+
                         System.out.println("Ingrese el mes de segunda fecha:");
                         segundoMes = scanner.nextInt();
                         System.out.println("Ingrese el día de segunda fecha:");
                         segundoDias = scanner.nextInt();
-        
+
                         if ((mesNac < 1 || mesNac > 12 || diaNac < 1 || diaNac > 30)
                                 || (segundoMes < 1 || segundoMes > 12 || segundoDias < 1 || segundoDias > 30)) {
                             System.out.println("Valores fuera de rango");
                         }
                     } while ((mesNac < 1 || mesNac > 12 || diaNac < 1 || diaNac > 30)
                             || (segundoMes < 1 || segundoMes > 12 || segundoDias < 1 || segundoDias > 30));
-        
+
                     edad = new Edad(mesNac, diaNac);
                     edad.calcularEdadConMesDias(segundoMes, segundoDias);
-        
+
                     break;
                 case 4:
                     do {
                         System.out.println("Ingrese la edad en años:");
                         edadEnAnios = scanner.nextByte();
                         if (edadEnAnios < 1 || edadEnAnios > 120) {
-        
+                         System.out.println("Valores fuera de rango");
                         }
                     } while (edadEnAnios < 1 || edadEnAnios > 120);
-        
+
                     edad = new Edad(edadEnAnios);
                     break;
                 default:
                     System.out.println("Opción no válida.");
             }
-        
+
         } while (edad == null);
     }
-}
